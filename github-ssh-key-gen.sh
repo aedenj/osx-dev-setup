dea195073ef1feec7dc361b070329cb57c1a0b7a
Aeden Jameson <aeden.jameson@gmail.com>
2019-10-15 21:43:15 -0700

    add ssh key to github account

---
+++ github-ssh-key-gen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+read -p "Email: ";
+email=$REPLY;
+
+read -p "Github Username: ";
+username=$REPLY;
+
+read -p "Key Name: ";
+name=$REPLY
+
+ssh-keygen -f "${HOME}/.ssh/id_rsa" -t rsa -b 4096 -C "${email}" -N ''
+sslpub="$(cat ${HOME}/.ssh/id_rsa.pub)"
+curl -u "${username}" \
+     --data "{\"title\":\"${name}\",\"key\":\"${sslpub}\"}" \
+     https://api.github.com/user/keys
+
