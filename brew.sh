2d3eca0d6252507e116bceee346ecad582395f84
Aeden Jameson <aeden.jameson@gmail.com>
2020-02-03 19:37:21 -0800

    add vscode repeat

--- brew.sh
+++ brew.sh
@@ -82,6 +82,9 @@ code --install-extension vscodevim.vim
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension scalameta.metals
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults delete -g ApplePressAndHoldEnabled # If necessary, reset global default
 
 
 brew install ack
