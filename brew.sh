2fec25c4553b049b8a6ab8ba3163584de27d11bc
Aeden Jameson <aeden.jameson@gmail.com>
2023-08-27 20:51:45 -0700

    cleanup unused tools

--- brew.sh
+++ brew.sh
@@ -16,6 +16,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew is already installed..."
 fi
 
 # Make sure we’re using the latest Homebrew.
@@ -34,10 +36,10 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
+brew install gnu-sed --with-default-names
 
 # ###############################################################################################
-# Install Bash 4.
+# Install Bash
 # ###############################################################################################
 brew install bash
 brew tap homebrew/core
@@ -66,37 +68,7 @@ brew install cask
 brew tap homebrew/cask-versions
 brew install brew-cask-completion
 
-# Development tools
-brew install --cask --appdir="/Applications" alfred
-brew install --cask --appdir="/Applications" iterm2
-
-# Java11 is needed by the scala metals plugin for vscode
-brew install --cask --appdir="~/Applications" java11
-brew install --cask --appdir="~/Applications" jdk-mission-control
-
-brew install --cask --appdir="/Applications" docker
-brew install --cask --appdir="/Applications" anaconda
-brew install --cask --appdir="/Applications" wireshark
-brew install --cask --appdir="/Applications" slack
-
-brew install --cask --appdir="/Applications" tor-browser
-brew install --cask --appdir="/Applications" brave-browser
-
-brew install --cask --appdir="/Applications" streamlabs-obs
-
-brew install --cask --appdir="/Applications" visual-studio-code
-code --install-extension --force vscodevim.vim
-code --install-extension --force ms-vscode.Go
-code --install-extension --force ms-vscode-remote.remote-containers
-code --install-extension --force scalameta.metals
-code --install-extension --force ms-kubernetes-tools.vscode-kubernetes-tools
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-defaults delete -g ApplePressAndHoldEnabled # If necessary, reset global default
-
-brew install --cask --appdir="/Applications" intellij-idea-ce
-defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
-
+# Install Basic Utils
 brew install bash-completion@2
 brew link --overwrite bash-completion@2
 
@@ -105,17 +77,21 @@ brew install git
 brew install gh
 brew install bash-git-prompt
 brew install jq
-brew install cmake macvim
 brew install telnet
 brew install csvkit
+brew install --cask --appdir="/Applications" iterm2
 
 
-# needed mostly for coc.nvim
-brew install nvm
-mkdir ~/.nvm
-nvm install node
+# Development tools
+brew install --cask --appdir="/Applications" docker
+brew install --cask --appdir="/Applications" wireshark
+brew install --cask --appdir="/Applications" slack
+brew install --cask --appdir="/Applications" brave-browser
 
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+brew install --cask --appdir="/Applications" intellij-idea
+defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
+
+brew install --cask --appdir="/Applications" jdk-mission-control
 
 brew install minikube
 brew install kubernetes-cli
@@ -125,9 +101,6 @@ brew install helm
 # Password management
 brew install --cask --appdir="/Applications" 1password
 brew install --cask 1password-cli
-
-# Google
-brew install --cask --appdir="/Applications" google-drive
 
 # Kafka Tools
 brew install kcat
