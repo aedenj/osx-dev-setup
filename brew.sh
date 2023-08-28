3990b619909cf3096eb21b1693e47726bfdcbe2b
Aeden Jameson <aeden.jameson@gmail.com>
2023-08-27 22:21:27 -0700

    remove change

--- brew.sh
+++ brew.sh
@@ -16,8 +16,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "Homebrew is already installed..."
 fi
 
 # Make sure we’re using the latest Homebrew.
