edb9e9a33dd5ba853f12ae42ca8035f7c110a9c1
Aeden Jameson <aeden@AedensMacStudio.localdomain>
2023-08-27 23:10:49 -0700

    update bash apps

--- configure-bash.sh
+++ configure-bash.sh
@@ -5,5 +5,5 @@ ln -s $OSXDEVSETUP_HOME/bash/gitconfig ~/.gitconfig
 mkdir ~/.bash
 ln -s $OSXDEVSETUP_HOME/bash/apps/aliases ~/.bash/aliases
 ln -s $OSXDEVSETUP_HOME/bash/apps/brew ~/.bash/brew
-ln -s $OSXDEVSETUP_HOME/bash/apps/gettext ~/.bash/gettext
+ln -s $OSXDEVSETUP_HOME/bash/apps/coreutils ~/.bash/coreutils
 ln -s $OSXDEVSETUP_HOME/bash/git-completion.bash ~/.bash/git-completion.bash
