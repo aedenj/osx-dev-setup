ed79268aa5539b721ba84ecd9108e70c89ec6c09
Aeden Jameson <aeden.jameson@gmail.com>
2023-09-04 10:49:03 -0700

    remove unneeded append

--- configure-bash.sh
+++ configure-bash.sh
@@ -2,14 +2,11 @@ export OSH="$HOME/.osxdevsetup/oh-my-bash"; bash -c "$(curl -fsSL https://raw.gi
 
 ln -s $OSXDEVSETUP_HOME/bash/bashrc  ~/.bashrc
 ln -s $OSXDEVSETUP_HOME/bash/bash_profile ~/.bash_profile
+ln -s $OSXDEVSETUP_HOME/bash/bashrc_ext ~/.bash/bashrc_ext
 ln -s $OSXDEVSETUP_HOME/bash/gitconfig ~/.gitconfig
 
 mkdir ~/.bash
-ln -s $OSXDEVSETUP_HOME/bash/bashrc_ext ~/.bash/bashrc_ext
 ln -s $OSXDEVSETUP_HOME/bash/apps/aliases ~/.bash/aliases
 ln -s $OSXDEVSETUP_HOME/bash/apps/brew ~/.bash/brew
 ln -s $OSXDEVSETUP_HOME/bash/apps/coreutils ~/.bash/coreutils
 ln -s $OSXDEVSETUP_HOME/bash/apps/java ~/.bash/java
-
-echo '' >> ~/.bashrc
-echo 'source ~/.bashrc_ext' >> ~/.bashrc
