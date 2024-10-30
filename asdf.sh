acbff293ecbf176a0a05e87a51c26750426fc33e
Aeden Jameson <aeden.jameson@gmail.com>
2024-10-30 01:31:41 -0700

    manage more through asdf

--- asdf.sh
+++ asdf.sh
@@ -11,6 +11,16 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf install java latest:temurin-11
+asdf install java latest:temurin-20
+asdf global java latest:temurin-20
 
-asdf install java latest:adoptopenjdk-11
-asdf install java latest:adoptopenjdk-21
+
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf install terraform latest
+asdf global terraform latest
+
+
+asdf plugin-add terragrunt
+asdf install terragrunt latest
+asdf global terragrunt latest
